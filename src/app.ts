11b6692f8fb7943202763858a6bbbb91e9d35573
Arsany Ashraf <arsanyarsany4800@gmail.com>
2024-09-11 19:00:58 +0300

    Add the api-docs and userRoutes

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
-require("dotenv").config();
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from './docs/swagger';
+import userRoutes from './routes/userRoutes'
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(express.static("public"));
-
-app.get("/", (req:Request, res:Response) => {
-  res.send("Hello World ");
+dotenv.config();
+// API documentation route
+app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api',userRoutes);
+app.get('/', async (req: Request, res: Response) => {
+    res.send('Hello World');
 });
 
 app.listen(port, () => {
