b787c0ee1166201734d65c5faab3efd40dcbb82b
saif-elhady <saifelhady2004@gmail.com>
2024-09-18 11:02:51 +0300

    Add login controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import prisma from '../prisma/prismaClient';
 import jwt from 'jsonwebtoken';
 import ApiError from '../middlewares/ApiError';
+import bcrypt from 'bcrypt';
 require('dotenv').config();
 
 export const signUp = async (req: Request, res: Response, next: NextFunction) => {
@@ -17,7 +18,6 @@ export const signUp = async (req: Request, res: Response, next: NextFunction) =>
                 email,
                 password: hashedPassword,
                 phone,
-                password,
             }
         })
 
@@ -25,11 +25,38 @@ export const signUp = async (req: Request, res: Response, next: NextFunction) =>
         if (!process.env.JWT_SECRET_KEY) {
             throw new Error('JWT_SECRET_KEY is not defined');
         }
-        const token = jwt.sign({userId:newUser.id}, process.env.JWT_SECRET_KEY);
+        const token = jwt.sign({ userId: newUser.id }, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
         res.status(201).json({ newUser, token });
     }
     catch (err){
         console.log(err);
-        return (new ApiError('signUp error', 501));
+        return (new ApiError('server error', 501));
+    }
+}
+
+export const login = async (req:Request, res:Response, next:NextFunction) => {
+    const { email, password } = req.body;
+    try {
+        const user = await prisma.users.findUnique({ where: { email: email } });
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return (new ApiError('Invalid email or password', 401));
+        }
+
+        //generate a jwt
+        if (!process.env.JWT_SECRET_KEY) {
+            throw new Error('JWT_SECRET_KEY is not defined');
+        }
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
+
+        if (!process.env.REFRESH_TOKEN_KEY) {
+            throw new Error('JWT_SECRET_KEY is not defined');
+        }
+        const refreshToken = jwt.sign({ userId: user.id }, process.env.REFRESH_TOKEN_KEY)
+
+        res.status(201).json({ token, refreshToken });
+    }
+    catch(err) {
+        if (err)
+            return (new ApiError('server error', 501))
     }
 }
