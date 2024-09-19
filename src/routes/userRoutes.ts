caf97346bd847381c767c1932896d84211e6e329
saif-elhady <saifelhady2004@gmail.com>
2024-09-19 22:50:41 +0300

    Add forgot password endpoint

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getAll, getOne, createOne, updateOne, deleteOne } from '../utils/crudUtils';
-import { login, signUp } from '../controllers/userController';
+import { forgotPassword, login, signUp } from '../controllers/userController';
 import { createUserValidator } from '../utils/vaildation/userValidation';
 import { authenticateRefreshToken } from '../controllers/refreshToken';
 
@@ -14,6 +14,7 @@ router.get('/users/:id', getOne('users'));
 router.post('/users/signup', createUserValidator, signUp);
 router.post('/users/login', login);
 router.post('/users/refresh-token', authenticateRefreshToken);
+router.post('/users/forgot-password', forgotPassword);
 router.put('/users/:id', updateOne('users'));
 router.delete('/users/:id', deleteOne('users'));
 
