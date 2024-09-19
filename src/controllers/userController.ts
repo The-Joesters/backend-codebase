229755238ddf6562b031877179cd61e793fef638
saif-elhady <saifelhady2004@gmail.com>
2024-09-19 22:53:53 +0300

    Add forgot password enpoint controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,7 +2,9 @@ import express, { Request, Response, NextFunction } from 'express';
 import prisma from '../prisma/prismaClient';
 import jwt from 'jsonwebtoken';
 import ApiError from '../middlewares/ApiError';
-import bcrypt from 'bcrypt';
+import bcrypt, { hash } from 'bcrypt';
+import crypto from 'crypto';
+import sendMail from '../utils/sendMail';
 require('dotenv').config();
 
 export const signUp = async (req: Request, res: Response, next: NextFunction) => {
@@ -10,7 +12,6 @@ export const signUp = async (req: Request, res: Response, next: NextFunction) =>
     try {
         //create new user
         const hashedPassword = await bcrypt.hash(password, 15);
-        console.log(hashedPassword);
         const newUser = await prisma.users.create({
             data: {
                 id,
@@ -39,7 +40,7 @@ export const login = async (req:Request, res:Response, next:NextFunction) => {
     try {
         const user = await prisma.users.findUnique({ where: { email: email } });
         if (!user || !(await bcrypt.compare(password, user.password))) {
-            return (new ApiError('Invalid email or password', 401));
+            return next(new ApiError('Invalid email or password', 401));
         }
 
         //generate a jwt
@@ -57,6 +58,32 @@ export const login = async (req:Request, res:Response, next:NextFunction) => {
     }
     catch(err) {
         if (err)
-            return (new ApiError('server error', 501))
+            return next(new ApiError('server error', 501))
+    }
+}
+
+export const forgotPassword = async (req: Request, res: Response, next: NextFunction) => {
+    const user = await prisma.users.findUnique({ where: { email: req.body.email } });
+        if (!user)
+            return next(new ApiError('user not found', 404));
+
+    //Generate a 4-digit reset code
+    const resetCode = Math.floor(1000 + Math.random() * 9000).toString();
+    user.resetCode = crypto.createHash('sha256').update(resetCode).digest('hex');
+    const message = `Your reset code is ${resetCode}`;
+    console.log(resetCode);
+
+    try {
+        await sendMail({
+            to: user.email,
+            subject: 'Reset Password',
+            text: message,
+            html: `<p>Your password reset code is <strong>${resetCode}</strong></p>`
+        })
+        res.status(200).json({ message: "reset code sent successfully" });
+    }
+    catch (err){
+        if (err)
+            return next(new ApiError('error sending email', 400))
     }
 }
