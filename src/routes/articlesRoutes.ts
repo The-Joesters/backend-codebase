fd3267b4b7c2de234e8db2d26e89bc4d0d6747c7
Arsany Ashraf <arsanyarsany4800@gmail.com>
2024-09-22 00:18:22 +0300

    Created and fixed all the functionality of the article routes, services

--- src/routes/articlesRoutes.ts
+++ src/routes/articlesRoutes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 import { createOne, getAll, getOne } from '../utils/crudUtils';
-import { StartArticle, UploadArticle } from '../controllers/articlesController';
+import { bookmarkArticle, deleteBookmark, FinishArticle, getArticlesCompleted, getArtilesInProgress, getBookmarkedArticles, StartArticle, StartArticleById, UploadArticle, ValidateId } from '../controllers/articlesController';
 
 const articleRoute: Router = Router()
-
-articleRoute.get("/articles", getAll('articles', () => {return true }));
-articleRoute.get("/articles/:id", getOne('articles'));
-articleRoute.post("/articles/upload", UploadArticle,StartArticle)
+articleRoute.get("/articles/completed", getArticlesCompleted);
+articleRoute.get("/articles/inprogress", getArtilesInProgress);
+articleRoute.get("/articles/bookmarked", getBookmarkedArticles);
+articleRoute.get("/articles", getAll('articles'));
+articleRoute.post("/articles/upload", UploadArticle, StartArticle);
+articleRoute.delete("/articles/bookmark/:ArticleId",deleteBookmark);
+articleRoute.post("/articles/bookmark/:ArticleId", bookmarkArticle);
+articleRoute.post("/articles/finish/:article_id", FinishArticle);
+articleRoute.post("/articles/:article_id", StartArticleById);
+articleRoute.get("/articles/:id", ValidateId, getOne('articles'));
 
 export default articleRoute;
--- src/services/articlesService.ts
+++ src/services/articlesService.ts
@@ -16,6 +16,72 @@ class articlesService {
         }
     }
 
+    async BookmarkArticle(userId: number, article_id: number) {
+        try {
+            const IsExistData = await prisma.bookmarked_articles.findFirst({
+                where: {
+                    user_id: userId,
+                    article_id: article_id
+                }
+            })
+            console.log("this is the data", IsExistData);
+            if (IsExistData) {
+                return IsExistData;
+            }
+            const data = await prisma.bookmarked_articles.create({
+                data: {
+                    user_id: userId,
+                    article_id: article_id
+                }
+            })
+            return data;
+        } catch (error) {
+            new ApiError("Error while creating the bookmark in db", 500);
+        }
+    }
+    async getBookMarked(userId: number) {
+        try {
+            const data = await prisma.bookmarked_articles.findMany({
+                where: {
+                    user_id: userId
+                }
+            })
+
+            return data;
+        } catch (error) {
+            new ApiError("Error while getting the books from the db", 500)
+        }
+    }
+    async checkBookmark(userId: number, article_id: number) {
+        try {
+
+            const data = await prisma.bookmarked_articles.findFirst({
+                where: {
+                    user_id: userId,
+                    article_id: article_id
+                }
+            })
+            return data;
+        } catch (error) {
+            new ApiError("Error while checking on the bookmark from db", 500);
+        }
+    }
+    async deleteBookmarkArticle(userId: number, article_id: number) {
+        try {
+
+            const data = await prisma.bookmarked_articles.delete({
+                where: {
+                    user_id_article_id: {
+                        user_id: userId,
+                        article_id: article_id
+                    }
+                }
+            })
+            return data;
+        } catch (error) {
+            new ApiError("Error while checking on the bookmark from db", 500);
+        }
+    }
 }
 
 export default new articlesService();
